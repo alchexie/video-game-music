5af2cdc9d47098569e118a5e2a4fd3414ef2e8a9
wanghuai <wanghuai@pwrd.com>
2026-03-27 13:08:38 +0800

    修改默认端口

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 6005,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8787',
+        target: 'http://127.0.0.1:5005',
         changeOrigin: true,
       },
     },
